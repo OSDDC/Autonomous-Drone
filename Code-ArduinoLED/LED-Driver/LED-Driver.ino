4b2543fd7fdc38a400b6635ff338b34a7ccf40b9
Marco <it.marco2807@gmail.com>
2023-09-20 09:12:56 +0200

    Update LED-Driver.ino

--- Code-ArduinoLED/LED-Driver/LED-Driver.ino
+++ Code-ArduinoLED/LED-Driver/LED-Driver.ino
@@ -8,7 +8,7 @@
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        4 // On Trinket or Gemma, suggest changing this to 1
+#define PIN        D4 // On Trinket or Gemma, suggest changing this to 1
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 8 // Popular NeoPixel ring size
