617e102c364af96e65280ad1fe7f9efd7ab43445
Marco <74458051+IT-TheCode@users.noreply.github.com>
2023-09-20 10:07:04 +0200

    Update LED-Driver.ino

--- Code-ArduinoLED/LED-Driver/LED-Driver.ino
+++ Code-ArduinoLED/LED-Driver/LED-Driver.ino
@@ -1,17 +1,10 @@
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// Released under the GPLv3 license to match the rest of the
-// Adafruit NeoPixel library
-
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        D4 // On Trinket or Gemma, suggest changing this to 1
+#define PIN        D4 // Neopixel PIN
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 8 // Popular NeoPixel ring size
+#define NUMPIXELS 8 // Neopixel Amount
 
 // When setting up the NeoPixel library, we tell it how many pixels,
 // and which pin to use to send signals. Note that for older NeoPixel
@@ -24,10 +17,6 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
   // Any other board, you can remove this part (but no harm leaving it):
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 }
