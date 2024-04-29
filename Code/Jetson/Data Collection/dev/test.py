a8cbd7387224657684a67eb6db47b08bc8304a32
Marco <74458051+IT-TheCode@users.noreply.github.com>
2024-04-29 16:52:26 +0200

    V3 Code

---
+++ Code/Jetson/Data Collection/dev/test.py
@@ -0,0 +1,33 @@
+# Complete Project Details: https://RandomNerdTutorials.com/raspberry-pi-analog-inputs-python-mcp3008/
+
+#from gpiozero import PWMLED, MCP3008
+from time import sleep
+
+
+#create an object called pot that refers to MCP3008 channel 0
+while True:
+    MQ9 = input("Enter the number for the variable:" )
+
+    print('Your input was=' + MQ9)
+
+    answer = input("Do you want to continue? (yes/no): ").lower()
+    if answer == "yes":
+        print("Continuing...")
+        # Put your code here that you want to execute if "yes" is entered
+
+        if MQ9 != 0:
+            outputtext = "{\n"
+            outputtext += " \"LPG\": " + MQ9 + ",\n"
+            outputtext += "}"
+        try:
+            with open("data.json", "a") as data:
+                data.write(",\n" + str(outputtext))
+        except:
+            with open("data.json", "w") as data:
+                data.write(str(outputtext))
+
+    elif answer == "no":
+        print("Exiting...")
+        break  # This will exit the loop and end the program
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
--- Code/Jetson/Data Collection/myfile.py
+++ Code/Jetson/Data Collection/myfile.py
@@ -1,29 +1,43 @@
 # Complete Project Details: https://RandomNerdTutorials.com/raspberry-pi-analog-inputs-python-mcp3008/
+# https://learn.adafruit.com/adafruit-sht31-d-temperature-and-humidity-sensor-breakout/python-circuitpython
+
 
 from gpiozero import PWMLED, MCP3008
 from time import sleep
+import board
+import adafruit_sht31d
 
+# sudo pip3 install adafruit-circuitpython-sht31d
 
+# Create sensor object, communicating over the board's default I2C bus
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+sensor = adafruit_sht31d.SHT31D(i2c)
 
 #create an object called pot that refers to MCP3008 channel 0
 MQ9 = MCP3008(0)
+Temp = 1
+Hum = 1
 
-
-while True:
-    def mq9():
+def main():
+    while True:
         print(MQ9.value)
         sleep(5)
         
-    mq9()
+            
+        if MQ9.value != 0:
+            outputtext = "{\n"
+            outputtext += " \"LPG\": " + MQ9.value + ",\n"
+            outputtext += " \"Temp\": " + sensor.temperature + ",\n"
+            outputtext += " \"Hum\": " + sensor.relative_humidity + ",\n"
+            outputtext += "}"
+        try:
+            with open("data.json", "a") as data:
+                data.write(",\n" + str(outputtext))
+        except:
+            with open("data.json", "w") as data:
+                data.write(str(outputtext))
 
 
-    if MQ9.value != 0:
-        outputtext = "{\n"
-        outputtext += " \"LPG\": " + pot.value + ",\n"
-        outputtext += "}"
-    try:
-        with open("data.json", "a") as data:
-            data.write(",\n" + str(outputtext))
-    except:
-        with open("data.json", "w") as data:
-            data.write(str(outputtext))
+if __name__ == "__main__":
+    main()
